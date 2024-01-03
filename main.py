b23e97147a34aefcd69ebe22e0abd97d21cc61da
mihaid150 <36085996+mihaid150@users.noreply.github.com>
2024-01-03 17:28:03 +0200

    created processes manager

--- main.py
+++ main.py
@@ -1,30 +1,14 @@
-
-import subprocess
-from io_parser import parse_fast_downward_output
+from processes_manager import run_py2pddl_init, run_py2pddl_parse, run_fast_downward
 
 # retrieve the domain and the problem
 domain = None
 problem = None
 
 # Save the domain to a file named 'domain.pddl'
-with open('domain.pddl', 'w') as domain_file:
+with open('pddl_resources/domain.pddl', 'w') as domain_file:
     domain_file.write(domain)
 
 # Save the problem to a file named 'problem.pddl'
-with open('problem.pddl', 'w') as problem_file:
+with open('pddl_resources/problem.pddl', 'w') as problem_file:
     problem_file.write(problem)
 
-# Define the command to be executed
-command = ["/home/mihai/tools/downward/fast-downward.py", "./domain.pddl", "./problem.pddl",
-           "--heuristic", "h=ff()", "--search", "astar(h)"]
-
-# Run the command
-process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-# Check if the process was successful
-if process.returncode == 0:
-    print("Command executed successfully!")
-    print(process.stdout)
-else:
-    print("Error in command execution.")
-    print("Error Output:\n", process.stderr)
---
+++ processes_manager.py
@@ -0,0 +1,56 @@
+import subprocess
+from io_parser import parse_fast_downward_output
+
+
+def run_py2pddl_init(file_name, domain_problem_name, types, predicates, actions):
+    # Define the command and inputs
+    command = ["python3", "-m", "py2pddl.init", file_name]
+    inputs = [
+        domain_problem_name + "\n",  # Name input followed by newline
+        types + "\n",  # Types input followed by newline
+        predicates + "\n",  # Predicates input followed by newline
+        actions + "\n"  # Actions input followed by newline
+    ]
+
+    # Start the subprocess and get the stdin, stdout, stderr
+    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, text=True)
+
+    # Send the inputs to the process and get the output
+    output, error = process.communicate(input="".join(inputs))
+
+    # Check for errors
+    if process.returncode != 0:
+        print(f"Error: {error}")
+    else:
+        print(f"Output: {output}")
+
+
+def run_py2pddl_parse(file_name):
+    # Define the command and inputs
+    command = ["python3", "-m", "py2pddl.parse", file_name]
+    # Start the subprocess and get the stdin, stdout, stderr
+    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, text=True)
+
+    if process.returncode != 0:
+        print(f"Error: {process.stderr}")
+    else:
+        print(f"Output: {process.stdout}")
+
+
+def run_fast_downward():
+    # Define the command to be executed
+    command = ["/home/mihai/tools/downward/fast-downward.py", "./pddl_resources/domain.pddl",
+               "./pddl_resources/problem.pddl", "--heuristic", "h=ff()", "--search", "astar(h)"]
+
+    # Run the command
+    process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # Check if the process was successful
+    if process.returncode == 0:
+        print("Command executed successfully!")
+        print(parse_fast_downward_output(process.stdout))
+    else:
+        print("Error in command execution.")
+        print("Error Output:\n", process.stderr)
