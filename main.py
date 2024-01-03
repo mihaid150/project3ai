df83a6f9fc2d107d109f41fc74a9ead4e0f093a5
mihaid150 <36085996+mihaid150@users.noreply.github.com>
2024-01-03 16:56:11 +0200

    updated main

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from pddl_domain_problem import PDDL
+
 import subprocess
 from io_parser import parse_fast_downward_output
 
-pddl = PDDL()
-
 # retrieve the domain and the problem
-domain = pddl.get_domain()
-problem = pddl.get_problem()
+domain = None
+problem = None
 
 # Save the domain to a file named 'domain.pddl'
 with open('domain.pddl', 'w') as domain_file:
@@ -26,7 +24,7 @@ process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
 # Check if the process was successful
 if process.returncode == 0:
     print("Command executed successfully!")
-    print(parse_fast_downward_output(process.stdout))
+    print(process.stdout)
 else:
     print("Error in command execution.")
     print("Error Output:\n", process.stderr)
